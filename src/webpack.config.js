c8133bb03703f04987a676394cef0e89bb3f0b5b
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-08-21 12:56:45 -0700

    refactor: remove all unneccessary assets and files

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -30,68 +30,96 @@ function getStyleUse(bundleFilename) {
 }
 
 module.exports = [{
-    entry: {
-        'bundle.min.css': [
-            // SCSS files contain imports that regard that section of the app 
-            // Main.scss contains imports that appear in multiple sections
-            path.resolve(__dirname, 'styles/main.scss'),
+        entry: {
+            'bundle.min.css': [
+                // SCSS files contain imports that regard that section of the app 
+                // Main.scss contains imports that appear in multiple sections
+                path.resolve(__dirname, 'styles/grid.scss'),
 
-            // All other styling files are contained as CSS files
-            path.resolve(__dirname, 'styles/menu.css'),
-            path.resolve(__dirname, 'styles/bootstrap.min.css'),
-        ]
-    },
-    output: {
-        // This is necessary for webpack to compile, but we never reference this js file.
-        filename: '[name]',
-        path: path.resolve(__dirname, '../build/styles/'),
-    },
-    module: {
-        rules: [{
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: [{
-                            loader: 'css-loader'
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                includePaths: [path.resolve(__dirname, 'node_modules/')]
+                // All other styling files are contained as CSS files
+                path.resolve(__dirname, 'styles/menu.css'),
+                path.resolve(__dirname, 'styles/footer.css'),
+                path.resolve(__dirname, 'styles/backtotop.css'),
+                path.resolve(__dirname, 'styles/bootstrap.min.css'),
+            ]
+        },
+        output: {
+            // This is necessary for webpack to compile, but we never reference this js file.
+            filename: '[name]',
+            path: path.resolve(__dirname, '../build/styles/'),
+        },
+        module: {
+            rules: [{
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({
+                        use: [{
+                                loader: 'css-loader'
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    includePaths: [path.resolve(__dirname, 'node_modules/')]
+                                }
                             }
-                        }
-                    ],
-                })
-            },
-            {
-                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=./assets/fonts/[hash].[ext]"
-            },
-            {
-                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=./assets/fonts/[hash].[ext]"
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=application/octet-stream&name=./assets/fonts/[hash].[ext]"
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "file-loader"
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: "url-loader?limit=10000&mimetype=image/svg+xml&name=./assets/fonts/[hash].[ext]"
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: 'css-loader'
-                })
-            }
-        ]
+                        ],
+                    })
+                },
+                {
+                    test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "url-loader?limit=10000&mimetype=application/font-woff&name=./assets/fonts/[hash].[ext]"
+                },
+                {
+                    test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "url-loader?limit=10000&mimetype=application/font-woff&name=./assets/fonts/[hash].[ext]"
+                },
+                {
+                    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "url-loader?limit=10000&mimetype=application/octet-stream&name=./assets/fonts/[hash].[ext]"
+                },
+                {
+                    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "file-loader"
+                },
+                {
+                    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                    loader: "url-loader?limit=10000&mimetype=image/svg+xml&name=./assets/fonts/[hash].[ext]"
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        use: 'css-loader'
+                    })
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin("bundle.min.css"),
+        ],
+        watch: true,
     },
-    plugins: [
-        new ExtractTextPlugin("bundle.min.css"),
-    ],
-    watch: true,
-}];
+    {
+        entry: {
+            'bundle.min.js': [
+                path.resolve(__dirname, 'scripts/custom.js'),
+                path.resolve(__dirname, 'scripts/menu.js'),
+            ]
+        },
+        output: {
+            // This is necessary for webpack to compile, but we never reference this js file.
+            filename: '[name]',
+            path: path.resolve(__dirname, '../build/scripts/'),
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    },
+                }],
+            }]
+        },
+        watch: true,
+    }
+];
