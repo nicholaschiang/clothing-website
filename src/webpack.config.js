b6b90f0ec30fe579b12a98b952a8e7b1e03d2a57
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-08-20 21:44:44 -0700

    initial commit

---
+++ src/webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function getStyleUse(bundleFilename) {
+    return [{
+            loader: 'file-loader',
+            options: {
+                name: bundleFilename,
+            },
+        }, {
+            loader: 'extract-loader'
+        },
+        {
+            loader: 'css-loader',
+        },
+        {
+            loader: 'postcss-loader',
+            options: {
+                plugins: () => [autoprefixer()]
+            }
+        },
+        {
+            loader: 'sass-loader',
+            options: {
+                includePaths: ['./node_modules'],
+            }
+        }
+    ];
+}
+
+module.exports = [{
+    entry: {
+        'bundle.min.css': [
+            // SCSS files contain imports that regard that section of the app 
+            // Main.scss contains imports that appear in multiple sections
+            path.resolve(__dirname, 'styles/main.scss'),
+
+            // All other styling files are contained as CSS files
+            path.resolve(__dirname, 'styles/menu.css'),
+            path.resolve(__dirname, 'styles/bootstrap.min.css'),
+        ]
+    },
+    output: {
+        // This is necessary for webpack to compile, but we never reference this js file.
+        filename: '[name]',
+        path: path.resolve(__dirname, '../build/styles/'),
+    },
+    module: {
+        rules: [{
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                includePaths: [path.resolve(__dirname, 'node_modules/')]
+                            }
+                        }
+                    ],
+                })
+            },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=./assets/fonts/[hash].[ext]"
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/font-woff&name=./assets/fonts/[hash].[ext]"
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=application/octet-stream&name=./assets/fonts/[hash].[ext]"
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "url-loader?limit=10000&mimetype=image/svg+xml&name=./assets/fonts/[hash].[ext]"
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("bundle.min.css"),
+    ],
+    watch: true,
+}];
